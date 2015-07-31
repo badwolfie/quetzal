25c079da28425e03bc78669d824d29cfc22a164b
Ian Hernández <ihernandezs@openmailbox.org>
2015-07-31 14:52:44 -0500

    License headers added to all source files.

--- src/qt-text-editor.h
+++ src/qt-text-editor.h
@@ -1,3 +1,21 @@
+/* qt-text-editor.h
+ *
+ * Copyright (C) 2015 Ian Hernandez <ihernandezs@openmailbox.org>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #ifndef QT_TEXT_EDITOR_H
 #define QT_TEXT_EDITOR_H
 
@@ -60,6 +78,5 @@ void qt_text_editor_set_color_scheme (QtTextEditor * self, const gchar * value);
 
 gboolean qt_text_editor_get_prefer_dark (QtTextEditor * self);
 void qt_text_editor_set_prefer_dark (QtTextEditor * self, gboolean value);
-
 
 #endif /* QT_TEXT_EDITOR_H */
